f19ca2f7df59f0f1bd6be0e046cf5fb14ca7c1c7
Artur Stępień <artur.stepien@bestproject.pl>
2023-03-10 16:20:19 +0100

    Added logo image size attributes in navbar.

--- template-parts/navbar.php
+++ template-parts/navbar.php
@@ -2,13 +2,22 @@
 
 use BestProject\NavWalker\Bootstrap5NavWalker;
 
+$logo = get_option('logo');
+$logo_media_id = attachment_url_to_postid($logo);
+if( is_int($logo_media_id) ) {
+    $logo = wp_get_attachment_image_src($logo_media_id);
+}
 ?>
 <header class="position-relative">
     <div class="navbar navbar-light navbar-expand-xl" id="nav">
         <div class="container">
             <a class="navbar-brand mr-0 mr-lg-60" href="<?php echo get_home_url() ?>">
-                <?php if (get_option('logo') !== ''): ?>
-                    <img src="<?php echo get_option('logo') ?>" class="navbar-brand-logo" alt="<?php bloginfo('name') ?> - <?php echo __('Logo', 'basetheme') ?>">
+                <?php if (is_string($logo) && $logo !== ''): ?>
+                    <img src="<?php echo $logo ?>" class="navbar-brand-logo" alt="<?php bloginfo('name') ?> - <?php echo __('Logo', 'basetheme') ?>">
+                <?php elseif( is_array($logo) && $logo!==[] ): ?>
+                    <img src="<?php echo $logo[0] ?>" width="<?php echo $logo[1] ?>" height="<?php echo $logo[2] ?>" class="navbar-brand-logo" alt="<?php bloginfo('name') ?> - <?php echo __('Logo', 'basetheme') ?>">
+                <?php else: ?>
+                    <?php bloginfo('name') ?>
                 <?php endif ?>
             </a>
             
