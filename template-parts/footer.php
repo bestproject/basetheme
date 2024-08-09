fa76cd0e757ad81d109299e5725e7b401c9172d8
Artur Stępień <artur.stepien@bestproject.pl>
2024-08-09 17:29:43 +0200

    Added default footer.

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -7,3 +7,10 @@
 extract($args, EXTR_OVERWRITE);
 
 ?>
+<footer>
+    <div class="container d-flex align-items-center justify-content-between">
+        <p class="mb-0 copyrights text-muted">
+            <?php echo date('Y') ?>&nbsp;&copy;&nbsp;<?php bloginfo('name') ?>
+        </p>
+    </div>
+</footer>
