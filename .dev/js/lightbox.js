cc3fbb829c9820f35bb72174c68a21a64043f6bc
Artur Stępień <artur.stepien@bestproject.pl>
2024-08-22 17:01:58 +0200

    Add wp-gallery support

--- .dev/js/lightbox.js
+++ .dev/js/lightbox.js
@@ -2,14 +2,30 @@ import $ from 'jquery';
 import 'magnific-popup';
 import 'magnific-popup/dist/magnific-popup.css';
 
-$(function(){
+$(()=>{
 
     const galleries = [];
 
+    // Collect and prepare wp-gallery
+    $('.wp-block-gallery').each(function(i,v){
+        const $gallery = $(v);
+        let galleryId;
+
+        if( $gallery.attr('id') ) {
+            galleryId = $gallery.attr('id');
+        } else {
+            galleryId = 'wp-gallery-'+Math.random().toString(32).substring(2);
+        }
+
+        $(v).find('a').attr('data-gallery', galleryId);
+    });
+
+    // Collect all galleries
     $('a[data-gallery]').each(function(i,v){
         galleries.push($(v).attr('data-gallery'));
     });
 
+    // Setup galleries for each galleries
     $.each(galleries, function(i,v){
         $('a[data-gallery="'+v+'"]').magnificPopup({
             type:'image',
@@ -22,6 +38,7 @@ $(function(){
         });
     });
 
+    // Setup single image lightboxes
     $("a[href$='.jpg']:not([data-gallery]),a[href$='.jpeg']:not([data-gallery]),a[href$='.png']:not([data-gallery]),a[href$='.gif']:not([data-gallery])").magnificPopup({
         type:'image',
         image: {
@@ -29,6 +46,7 @@ $(function(){
         },
     });
 
+    // Setup YouTube lightbox
     $("a[href*='youtube.']").magnificPopup({
         disableOn: 700,
         type: 'iframe',
