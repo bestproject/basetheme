7eb1ffd97957ec027a8d405ba75dea960d37d12d
Artur Stępień <artur.stepien@bestproject.pl>
2024-09-19 18:15:21 +0200

    Register build-in blocks

--- src/BaseTheme/Block/Bootstrap.php
+++ src/BaseTheme/Block/Bootstrap.php
@@ -2,15 +2,31 @@
 
 namespace BaseTheme\Block;
 
+use Exception;
+
 /**
  * Custom Boostrap blocks for WordPress.
  */
 class Bootstrap
 {
+    /**
+     * @throws Exception
+     */
     public static function register(): void
     {
-        register_block_type(get_template_directory() . '/blocks/wp/container');
-        register_block_type(get_template_directory() . '/blocks/wp/row');
-        register_block_type(get_template_directory() . '/blocks/wp/column');
+        self::registerBlock(get_template_directory() . '/blocks/wp/container');
+        self::registerBlock(get_template_directory() . '/blocks/wp/row');
+        self::registerBlock(get_template_directory() . '/blocks/wp/column');
+    }
+
+    /**
+     * @throws Exception
+     */
+    private static function registerBlock(string $path): void {
+        if( is_dir($path) ) {
+            register_block_type($path);
+        } else {
+            throw new \RuntimeException('Block ' . $path . ' does not exist.');
+        }
     }
 }
