56d791a74e739afa2de72f65ed5f3f93e76ea2ca
Artur Stępień <artur.stepien@bestproject.pl>
2026-02-25 14:25:36 +0100

    Fixing SVG support

--- src/BestProject/Helper/MediaHelper.php
+++ src/BestProject/Helper/MediaHelper.php
@@ -23,8 +23,15 @@ class MediaHelper
 
         $attributes['src'] = $details[0];
         $attributes['alt'] = get_post_meta( $attachment_id, '_wp_attachment_image_alt', true );
-        $attributes['width'] = $details[1];
-        $attributes['height'] = $details[2];
+
+        if( $details[1]>1 ) {
+            $attributes['width'] = $details[1];
+        }
+
+        if( $details[2]>1 ) {
+            $attributes['height'] = $details[2];
+        }
+
         $attributes = array_merge($attributes, $attribs);
 
         return '<img '.ArrayHelper::toAttributes($attributes).' />';
