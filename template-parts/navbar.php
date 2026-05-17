d06ccf1f47bfcd2dbcb1e49ba9471a630de29f45
Artur Stępień <artur.stepien@bestproject.pl>
2026-05-17 17:03:41 +0200

    Removed unnecessary H4 in menu

--- template-parts/navbar.php
+++ template-parts/navbar.php
@@ -45,7 +45,7 @@ if( is_int($logo_media_id) ) {
 
                 <div class="offcanvas offcanvas-end d-xl-none" tabindex="-1" id="offcanvas-navigation" aria-labelledby="offcanvas-navigation-toggle">
                     <div class="offcanvas-header px-3 py-3 d-flex justify-content-between border-bottom mb-3">
-                        <h4 class="h5 ms-3 mb-0"><?php echo __('Menu', 'bestproject') ?></h4>
+                        <p class="h5 ms-3 mb-0"><?php echo __('Menu', 'bestproject') ?></p>
                         <button data-bs-dismiss="offcanvas" class="btn btn-outline-primary btn-sm p-3 offcanvas-navigation-toggle d-flex align-items-center" aria-label="<?php echo __('Close') ?>">
                             <i class="fas fa-times" aria-hidden="true"><span></span></i><span class="visually-hidden" aria-hidden="true"><?php echo __('Close menu', 'bestproject') ?></span>
                         </button>
